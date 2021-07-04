3bfaf6145bc9a20a89066e626957eb34919e717d
James Liu <james@jamesliu.io>
2021-07-04 17:44:43 +1000

    feat: draw and fill a simple window
    Following lesson 1:
    https://lazyfoo.net/tutorials/SDL/01_hello_SDL/index2.php
    
    Note that a rudimentary event loop was used instead of calling
    SDL_Delay() as shown in the tutorial because the window fails to
    render on macOS otherwise. See
    https://discourse.libsdl.org/t/sdl2-cant-see-the-window/25835/9
    for an explanation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,57 @@
-#include <stdio.h>
+#include <cstdio>
+#include <SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+void fail(const char* reason) {
+    printf("%s", reason);
+    EXIT_FAILURE;
+}
 
 int main() {
-    printf("Hello World\n");
+    SDL_Window* window = nullptr;
+    SDL_Surface* screen_surface = nullptr;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        char* error;
+        asprintf(&error, "SDL could not initialise! SDL_Error: %s\n", SDL_GetError());
+        fail(error);
+    }
+
+    window = SDL_CreateWindow("SDL Tutorial",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              SCREEN_WIDTH,
+                              SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN);
+
+    if (window == nullptr) {
+        char* error;
+        asprintf(&error, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        fail(error);
+    }
+
+    screen_surface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screen_surface,
+                 nullptr,
+                 SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+
+    // The window needs to be updated before anything will show.
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Event e;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT || e.type == SDL_MOUSEBUTTONDOWN){
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
